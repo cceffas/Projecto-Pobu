3ba6ea98f72b26a4a5a1b262254ce118bf1beaae
devcefas <163452765+Fhinker-Developer@users.noreply.github.com>
2025-01-22 19:58:40 +0000

    handlebar configurado

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const { engine } = require("express-handlebars");
+const DIR = require("path");
+
 const RouteSystem = require("./routes/routes");
+const { log } = require("console");
 
 const servidor = express();
 
+console.log()
+servidor.engine("handlebars", engine());
+servidor.set("view engine", "handlebars");
+servidor.set("views",DIR.join(__dirname,'sources/views'))
+
+
+
+
+
+
 const Routas = new RouteSystem(servidor);
 
-Routas.GET("/", "index");
+servidor.get('/',function(req,res){
+
+    res.render('home',{titulo:'pobuProject'})
+})
+// Routas.GET("/", "index");
 Routas.GET("/home", "auth/home");
 
 const porta = 3000;
